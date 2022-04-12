00abdd37ccafb7c5d4e54c3ba1ecdcb2eb5f34c1
Hleb <godads@yandex.ru>
2022-04-12 19:21:12 +0400

    i

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,20 @@
+<template>
+  <div class="container mx-auto mt-4">
+    <router-view></router-view>
+    <div v-if="loading">
+      <h3 class="text-center mt-4">Loading...</h3>
+    </div>
+    <div v-else>
+      <NewItem />
+      <TodoList :filter="route.path" />
+      <FooterButtons />
+    </div>
+  </div>
+</template>
+
 <script lang="ts">
 import { computed, defineComponent, onMounted } from 'vue'
+import { useRoute } from 'vue-router'
 import FooterButtons from './components/FooterButtons.vue'
 import NewItem from './components/NewItem.vue'
 import TodoList from './components/TodoList.vue'
@@ -9,25 +24,11 @@ import { ActionTypes } from './store/actions'
 export default defineComponent({
   components: { TodoList, NewItem, FooterButtons },
   setup() {
+    const route = useRoute();
     const store = useStore()
     const loading = computed(() => store.state.loading)
     onMounted(() => store.dispatch(ActionTypes.GetTodoItems))
-    return { loading }
+    return { loading, route }
   }
 })
 </script>
-
-<template>
-  <div class="container mx-auto mt-4">
-    <router-view></router-view>
-    <div v-if="loading">
-      <h3 class="text-center mt-4">Loading...</h3>
-    </div>
-    <div v-else>
-      <NewItem />
-      <TodoList />
-      <FooterButtons />
-    </div>
-
-  </div>
-</template>
